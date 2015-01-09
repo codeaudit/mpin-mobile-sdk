4841e96f30e9bc9d8054b46165bbccb1adbdf1b2
slav.klenov <slav.klenov@certivox.com>
2015-01-09 12:02:10 +0200

    Added using namespace std.

--- cpp/windows/CvHttpRequest.cpp
+++ cpp/windows/CvHttpRequest.cpp
@@ -46,11 +46,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 #include <stdlib.h>
 
 using namespace CvShared;
-using std::string;
-//typedef std::string string;
-typedef std::wstring wstring;
-typedef std::runtime_error runtime_error;
-typedef std::exception exception;
+using namespace std;
 
 CvHttpRequest::CvHttpRequest( enHttpMethod_t method ) :
 	m_bCancel(false), m_responseCode(0), m_progressUp(0), m_progressDown(0),
