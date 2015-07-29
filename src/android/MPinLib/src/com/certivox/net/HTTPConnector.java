451e86bd872dc66897923e88f27fdfdf5fa02e90
bogi <bogoi.bogdanov@certivox.com>
2015-07-29 16:09:26 +0300

    Fix bug MPINMOB-354

--- src/android/MPinLib/src/com/certivox/net/HTTPConnector.java
+++ src/android/MPinLib/src/com/certivox/net/HTTPConnector.java
@@ -1,5 +1,6 @@
 package com.certivox.net;
 
+
 import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,271 +17,296 @@ import java.util.Map;
 import android.net.Uri;
 import android.text.TextUtils;
 
+
 public class HTTPConnector implements IHTTPRequest {
 
-	private Hashtable<String, String> requestHeaders;
-	private Hashtable<String, String> queryParams;
-	private String requestBody;
-	private int timeout = DEFAULT_TIMEOUT;
-	private String errorMessage;
-	private int statusCode;
-	private Hashtable<String, String> responseHeaders;
-	private String responseData;
+    private Hashtable<String, String> requestHeaders;
+    private Hashtable<String, String> queryParams;
+    private String                    requestBody;
+    private int                       timeout = DEFAULT_TIMEOUT;
+    private String                    errorMessage;
+    private int                       statusCode;
+    private Hashtable<String, String> responseHeaders;
+    private String                    responseData;
 
-	// / only for test !!!!
-	public String getContent() {
 
-		return requestBody;
-	}
+    // / only for test !!!!
+    public String getContent() {
 
-	public Hashtable<String, String> RequestHeaders() {
-		return requestHeaders;
-	}
+        return requestBody;
+    }
 
-	public HTTPConnector() {
-		super();
-	}
 
-	protected HttpURLConnection getConnection(String serviceURL, boolean output)
-			throws MalformedURLException, IOException {
-		if (serviceURL.startsWith("/"))
-			serviceURL = "http://ec2-54-77-232-113.eu-west-1.compute.amazonaws.com"
-					+ serviceURL;
-		HttpURLConnection httpConnection = (HttpURLConnection) new URL(
-				serviceURL).openConnection();
-		httpConnection.setDoInput(true);
-		httpConnection.setDoOutput(output);
-		return httpConnection;
-	}
+    public Hashtable<String, String> RequestHeaders() {
+        return requestHeaders;
+    }
 
-	protected String HttpMethodMapper(int method) {
-		if (GET == method)
-			return HTTP_GET;
-		else if (POST == method)
-			return HTTP_POST;
-		else if (PUT == method)
-			return HTTP_PUT;
-		else if (DELETE == method)
-			return HTTP_DELETE;
-		else if (OPTIONS == method)
-			return HTTP_OPTIONS;
-		else
-			return HTTP_PATCH;
-	}
 
-	protected String sendRequest(String serviceURL, String http_method,
-			String requestBody, Hashtable<String, String> requestProperties)
-			throws IOException, HTTPErrorException {
+    public HTTPConnector() {
+        super();
+    }
 
-		HttpURLConnection connection = null;
-		DataOutputStream dos = null;
-		String response = "200 OK";
 
-		try {
-			connection = getConnection(serviceURL,
-					!TextUtils.isEmpty(requestBody));
+    protected HttpURLConnection getConnection(String serviceURL, boolean output) throws MalformedURLException,
+            IOException {
+        if (serviceURL.startsWith("/")) {
+            serviceURL = "http://ec2-54-77-232-113.eu-west-1.compute.amazonaws.com" + serviceURL;
+        }
 
-			connection.setRequestMethod(http_method);
-			connection.setConnectTimeout(timeout);
+        HttpURLConnection httpConnection = (HttpURLConnection) new URL(serviceURL).openConnection();
+        httpConnection.setDoInput(true);
+        httpConnection.setDoOutput(output);
+        return httpConnection;
+    }
 
-			if (requestProperties != null) {
-				if (!requestProperties.isEmpty()) {
-					Enumeration<String> keyEnum = requestProperties.keys();
-					while (keyEnum.hasMoreElements()) {
-						String key = keyEnum.nextElement();
-						connection.setRequestProperty(key,
-								requestProperties.get(key));
-					}
-				}
-			}
 
-			if (!TextUtils.isEmpty(requestBody)) {
-				dos = new DataOutputStream(connection.getOutputStream());
-				dos.writeBytes(requestBody);
-			}
+    protected String HttpMethodMapper(int method) {
+        if (GET == method)
+            return HTTP_GET;
+        else
+            if (POST == method)
+                return HTTP_POST;
+            else
+                if (PUT == method)
+                    return HTTP_PUT;
+                else
+                    if (DELETE == method)
+                        return HTTP_DELETE;
+                    else
+                        if (OPTIONS == method)
+                            return HTTP_OPTIONS;
+                        else
+                            return HTTP_PATCH;
+    }
 
-			// Starts the query
-			connection.connect();
 
-			try {
-				statusCode = connection.getResponseCode();
-			} catch (IOException e) {
-				statusCode = connection.getResponseCode();
-				if (statusCode != 401) {
-					throw e;
-				}
-			}
+    protected String sendRequest(String serviceURL, String http_method, String requestBody,
+            Hashtable<String, String> requestProperties) throws IOException, HTTPErrorException {
 
-			responseHeaders = new Hashtable<String, String>();
-			Map<String, List<String>> map = connection.getHeaderFields();
-			for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-				List<String> propertyList = entry.getValue();
-				String properties = "";
-				for (String s : propertyList) {
-					properties += s;
-				}
-				String key = entry.getKey();
-				if (key == null)
-					continue;
-				responseHeaders.put(entry.getKey(), properties);
-			}
-			response = toString(connection.getInputStream());
+        HttpURLConnection connection = null;
+        DataOutputStream dos = null;
+        String response = "200 OK";
 
-		} finally {
-			if (dos != null) {
-				dos.close();
-			}
-			if (connection != null) {
-				connection.disconnect();
-			}
-		}
+        try {
+            connection = getConnection(serviceURL, !TextUtils.isEmpty(requestBody));
 
-		return response;
-	}
+            connection.setRequestMethod(http_method);
+            connection.setConnectTimeout(timeout);
 
-	protected String sendRequest(String serviceURL, String http_method,
-			String requestBody) throws IOException, HTTPErrorException {
-		return sendRequest(serviceURL, http_method, requestBody, null);
-	}
+            if (requestProperties != null) {
+                if (!requestProperties.isEmpty()) {
+                    Enumeration<String> keyEnum = requestProperties.keys();
+                    while (keyEnum.hasMoreElements()) {
+                        String key = keyEnum.nextElement();
+                        connection.setRequestProperty(key, requestProperties.get(key));
+                    }
+                }
+            }
 
-	protected String sendRequest(String serviceURL, String http_method)
-			throws IOException, HTTPErrorException {
-		return sendRequest(serviceURL, http_method, null);
-	}
+            if (!TextUtils.isEmpty(requestBody)) {
+                dos = new DataOutputStream(connection.getOutputStream());
+                dos.writeBytes(requestBody);
+            }
 
-	@Override
-	public void SetHeaders(Hashtable<String, String> headers) {
-		this.requestHeaders = headers;
-	}
+            // Starts the query
+            connection.connect();
 
-	@Override
-	public void SetQueryParams(Hashtable<String, String> queryParams) {
-		this.queryParams = queryParams;
-	}
+            try {
+                statusCode = connection.getResponseCode();
+            } catch (IOException e) {
+                statusCode = connection.getResponseCode();
+                if (statusCode != 401) {
+                    throw e;
+                }
+            }
 
-	@Override
-	public void SetContent(String data) {
-		this.requestBody = data;
-	}
+            responseHeaders = new Hashtable<String, String>();
+            Map<String, List<String>> map = connection.getHeaderFields();
+            for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+                List<String> propertyList = entry.getValue();
+                String properties = "";
+                for (String s : propertyList) {
+                    properties += s;
+                }
+                String key = entry.getKey();
+                if (key == null)
+                    continue;
+                responseHeaders.put(entry.getKey(), properties);
+            }
+            response = toString(connection.getInputStream());
 
-	@Override
-	public void SetTimeout(int seconds) {
-		if (seconds <= 0)
-			throw new IllegalArgumentException();
-		this.timeout = seconds;
-	}
+        } finally {
+            if (dos != null) {
+                dos.close();
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
 
-	@Override
-	public boolean Execute(int method, String url) {
-		if (TextUtils.isEmpty(url))
-			throw new IllegalArgumentException();
+        return response;
+    }
 
-		String fullUrl = url;
-		if (queryParams != null) {
-			if (!queryParams.isEmpty()) {
-				Enumeration<String> keyEnum = queryParams.keys();
-				fullUrl += "?";
-				while (keyEnum.hasMoreElements()) {
-					String key = keyEnum.nextElement();
-					fullUrl = key + "=" + queryParams.get(key) + "&";
-				}
-				fullUrl = fullUrl.substring(0, fullUrl.length() - 1);
-			}
-		}
 
-		// TODO temporary hack
-		Uri uri = Uri.parse(fullUrl);
-		if ("wss".equals(uri.getScheme()))
-			fullUrl = uri.buildUpon().scheme("https").build().toString();
+    protected String sendRequest(String serviceURL, String http_method, String requestBody) throws IOException,
+            HTTPErrorException {
+        return sendRequest(serviceURL, http_method, requestBody, null);
+    }
 
-		try {
-			responseData = sendRequest(fullUrl, HttpMethodMapper(method),
-					requestBody, requestHeaders);
-		} catch (FileNotFoundException e) {
-			// No data in response
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-			errorMessage = e.getLocalizedMessage();
-			return false;
-		}
 
-		return true;
-	}
+    protected String sendRequest(String serviceURL, String http_method) throws IOException, HTTPErrorException {
+        return sendRequest(serviceURL, http_method, null);
+    }
 
-	@Override
-	public String GetExecuteErrorMessage() {
-		return errorMessage;
-	}
 
-	@Override
-	public int GetHttpStatusCode() {
-		return statusCode;
-	}
+    @Override
+    public void SetHeaders(Hashtable<String, String> headers) {
+        this.requestHeaders = headers;
+    }
 
-	@Override
-	public Hashtable<String, String> GetResponseHeaders() {
-		return responseHeaders;
-	}
 
-	@Override
-	public String GetResponseData() {
-		return responseData;
-	}
+    @Override
+    public void SetQueryParams(Hashtable<String, String> queryParams) {
+        this.queryParams = queryParams;
+    }
 
-	@SuppressWarnings("serial")
-	public class HTTPErrorException extends Exception {
 
-		private int statusCode;
+    @Override
+    public void SetContent(String data) {
+        this.requestBody = data;
+    }
 
-		public HTTPErrorException() {
-			// TODO Auto-generated constructor stub
-		}
 
-		public HTTPErrorException(String message) {
-			super(message);
-			// TODO Auto-generated constructor stub
-		}
+    @Override
+    public void SetTimeout(int seconds) {
+        if (seconds <= 0)
+            throw new IllegalArgumentException();
+        this.timeout = seconds;
+    }
 
-		public HTTPErrorException(String message, int statusCode) {
-			super(message);
-			setStatusCode(statusCode);
-		}
 
-		public HTTPErrorException(Throwable cause) {
-			super(cause);
-			// TODO Auto-generated constructor stub
-		}
+    @Override
+    public boolean Execute(int method, String url) {
+        if (TextUtils.isEmpty(url))
+            throw new IllegalArgumentException();
 
-		public HTTPErrorException(String message, Throwable cause) {
-			super(message, cause);
-			// TODO Auto-generated constructor stub
-		}
+        String fullUrl = url;
+        if (queryParams != null) {
+            if (!queryParams.isEmpty()) {
+                Enumeration<String> keyEnum = queryParams.keys();
+                fullUrl += "?";
+                while (keyEnum.hasMoreElements()) {
+                    String key = keyEnum.nextElement();
+                    fullUrl = key + "=" + queryParams.get(key) + "&";
+                }
+                fullUrl = fullUrl.substring(0, fullUrl.length() - 1);
+            }
+        }
 
-		public int getStatusCode() {
-			return statusCode;
-		}
+        // TODO temporary hack
+        Uri uri = Uri.parse(fullUrl);
+        if ("wss".equals(uri.getScheme()))
+            fullUrl = uri.buildUpon().scheme("https").build().toString();
 
-		public void setStatusCode(int statusCode) {
-			this.statusCode = statusCode;
-		}
-	}
+        try {
+            responseData = sendRequest(fullUrl, HttpMethodMapper(method), requestBody, requestHeaders);
+        } catch (FileNotFoundException e) {
+            // No data in response
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+            errorMessage = e.getLocalizedMessage();
+            return false;
+        }
 
-	private static String toString(InputStream is) throws IOException {
-		InputStreamReader isr = null;
-		try {
-			isr = new InputStreamReader(is, "UTF-8");
-			char[] buf = new char[512];
-			StringBuilder str = new StringBuilder();
-			int i = 0;
-			while ((i = isr.read(buf)) != -1)
-				str.append(buf, 0, i);
-			return str.toString();
-		} finally {
-			if (isr != null) {
-				isr.close();
-			}
-		}
-	}
+        return true;
+    }
+
+
+    @Override
+    public String GetExecuteErrorMessage() {
+        return errorMessage;
+    }
+
+
+    @Override
+    public int GetHttpStatusCode() {
+        return statusCode;
+    }
+
+
+    @Override
+    public Hashtable<String, String> GetResponseHeaders() {
+        return responseHeaders;
+    }
+
+
+    @Override
+    public String GetResponseData() {
+        return responseData;
+    }
+
+    @SuppressWarnings("serial")
+    public class HTTPErrorException extends Exception {
+
+        private int statusCode;
+
+
+        public HTTPErrorException() {
+            // TODO Auto-generated constructor stub
+        }
+
+
+        public HTTPErrorException(String message) {
+            super(message);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        public HTTPErrorException(String message, int statusCode) {
+            super(message);
+            setStatusCode(statusCode);
+        }
+
+
+        public HTTPErrorException(Throwable cause) {
+            super(cause);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        public HTTPErrorException(String message, Throwable cause) {
+            super(message, cause);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        public int getStatusCode() {
+            return statusCode;
+        }
+
+
+        public void setStatusCode(int statusCode) {
+            this.statusCode = statusCode;
+        }
+    }
+
+
+    private static String toString(InputStream is) throws IOException {
+        InputStreamReader isr = null;
+        try {
+            isr = new InputStreamReader(is, "UTF-8");
+            char[] buf = new char[512];
+            StringBuilder str = new StringBuilder();
+            int i = 0;
+            while ((i = isr.read(buf)) != -1)
+                str.append(buf, 0, i);
+            return str.toString();
+        } finally {
+            if (isr != null) {
+                isr.close();
+            }
+        }
+    }
 
 }
