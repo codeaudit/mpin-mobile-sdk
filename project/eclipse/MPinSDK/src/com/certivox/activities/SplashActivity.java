9e8c01cd98819fb4fb215ae95cc85bde3e47620a
bogi <bogoi.bogdanov@certivox.com>
2015-08-17 11:37:42 +0300

    Create splash activity

---
+++ project/eclipse/MPinSDK/src/com/certivox/activities/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.certivox.activities;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
