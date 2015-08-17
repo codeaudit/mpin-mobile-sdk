257d8c92953cd34959ba429091c148b967f251af
Vassil Angelov <vassil.angelov@certivox.com>
2015-08-17 17:41:40 +0300

    Add initial layouts for the guide activity

--- project/eclipse/MPinSDK/src/com/certivox/activities/SplashActivity.java
+++ project/eclipse/MPinSDK/src/com/certivox/activities/SplashActivity.java
@@ -1,17 +1,15 @@
 package com.certivox.activities;
 
-
 import java.util.ArrayList;
-import java.util.List;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 import com.certivox.constants.IntentConstants;
 import com.certivox.dal.InstructionsDao;
 import com.certivox.enums.GuideFragmentsEnum;
+import com.certivox.mpinsdk.R;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 public class SplashActivity extends ActionBarActivity {
 
@@ -21,6 +19,7 @@ public class SplashActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
         mInstructionsDao = new InstructionsDao(getApplicationContext());
         setInitialActivity();
     }
--- project/eclipse/MPinSDK/src/com/certivox/enums/GuideFragmentsEnum.java
+++ project/eclipse/MPinSDK/src/com/certivox/enums/GuideFragmentsEnum.java
@@ -5,7 +5,7 @@ import com.certivox.mpinsdk.R;
 
 
 public enum GuideFragmentsEnum {
-    FRAGMENT_1(R.layout.fragment_guide), FRAGMENT_2(R.layout.fragment_guide), FRAGMENT_3(R.layout.fragment_guide), FRAGMENT_4(
+    FRAGMENT_1(R.layout.fragment_gd_create_identity), FRAGMENT_2(R.layout.fragment_guide), FRAGMENT_3(R.layout.fragment_guide), FRAGMENT_4(
             R.layout.fragment_guide);
 
     private int mResourceId;
