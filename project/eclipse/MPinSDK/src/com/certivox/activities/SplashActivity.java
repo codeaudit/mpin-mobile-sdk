e34d05a4ba536977e700c621820b2c39c2bf6b2a
bogi <bogoi.bogdanov@certivox.com>
2015-08-17 12:00:12 +0300

    Implement splash activity to be the initial acitivity. Create DAO object for checking and setting if the application was started for a first time.

--- project/eclipse/MPinSDK/src/com/certivox/activities/SplashActivity.java
+++ project/eclipse/MPinSDK/src/com/certivox/activities/SplashActivity.java
@@ -1,14 +1,34 @@
 package com.certivox.activities;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+
+import com.certivox.dal.InstructionsDao;
 
 
 public class SplashActivity extends ActionBarActivity {
 
+    private InstructionsDao mInstructionsDao;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mInstructionsDao = new InstructionsDao(getApplicationContext());
+        setInitialActivity();
+    }
+
+
+    private void setInitialActivity() {
+        Intent intent;
+        if (mInstructionsDao.isFirstStart()) {
+            mInstructionsDao.setIsFirstStart(false);
+        } else {
+            intent = new Intent(this, MPinActivity.class);
+            startActivity(intent);
+        }
+        finish();
     }
 }
