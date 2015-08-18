6d98dd58e3ed2ed8be748c6e71ff27b5734baf86
bogi <bogoi.bogdanov@certivox.com>
2015-08-18 10:36:39 +0300

    Show done button when guide pager is at the end

--- project/eclipse/MPinSDK/src/com/certivox/activities/GuideActivity.java
+++ project/eclipse/MPinSDK/src/com/certivox/activities/GuideActivity.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,11 +21,13 @@ import com.certivox.mpinsdk.R;
 
 public class GuideActivity extends ActionBarActivity {
 
-    private ViewPager   mViewPager;
+    private static final String TAG = GuideActivity.class.getCanonicalName();
 
-    private Button      mDoneButton;
-    private ImageButton mNextButton;
-    private Button      mSkipButton;
+    private ViewPager           mViewPager;
+
+    private Button              mDoneButton;
+    private ImageButton         mNextButton;
+    private Button              mSkipButton;
 
 
     @Override
@@ -42,7 +45,6 @@ public class GuideActivity extends ActionBarActivity {
         mDoneButton = (Button) findViewById(R.id.guide_done_button);
         mNextButton = (ImageButton) findViewById(R.id.guide_next_button);
         mSkipButton = (Button) findViewById(R.id.guide_skip_button);
-
         mViewPager = (ViewPager) findViewById(R.id.pager);
     }
 
@@ -54,6 +56,38 @@ public class GuideActivity extends ActionBarActivity {
                 .getSerializableExtra(IntentConstants.FRAGMENT_LIST);
         GuidePagerAdapter guidePagerAdapter = new GuidePagerAdapter(fragmentList, getSupportFragmentManager());
         mViewPager.setAdapter(guidePagerAdapter);
+        mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int pageNumber) {
+                GuideActivity.this.showProperButtons(mViewPager.getAdapter().getCount() - 1 == pageNumber);
+            }
+
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                //Do nothing
+            }
+
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                //Do nothing
+            }
+        });
+
+        showProperButtons(fragmentList.size() <= 1);
+    }
+
+
+    private void showProperButtons(boolean isEndOfPage) {
+        if (isEndOfPage) {
+            hideNextButton();
+            showDoneButton();
+        } else {
+            hideDoneButton();
+            showNextButton();
+        }
     }
 
 
@@ -83,4 +117,23 @@ public class GuideActivity extends ActionBarActivity {
         });
     }
 
+
+    private void showDoneButton() {
+        mDoneButton.setVisibility(View.VISIBLE);
+    }
+
+
+    private void hideDoneButton() {
+        mDoneButton.setVisibility(View.INVISIBLE);
+    }
+
+
+    private void showNextButton() {
+        mNextButton.setVisibility(View.VISIBLE);
+    }
+
+
+    private void hideNextButton() {
+        mNextButton.setVisibility(View.INVISIBLE);
+    }
 }
